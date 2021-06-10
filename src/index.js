76535326a2c0e727ec2541c76ae8bf0736f5edfe
subhasis020299 <subhasisdas125@gmail.com>
2021-06-10 22:36:27 +0530

    checking or 45+

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const sendEmail = require("./utils/email");
 //declaring constants - change according to your needs:
 const STATE = "WEST BENGAL"; // spelling should be correct
 const DISTRICT = "KOLKATA"; // full name of the district (as mentioned in the dropdown menu of CoWin website)
-const MIN_AGE = [18, 45]; // can also be set to only [18] or only [45] as per your need
+const MIN_AGE = [45]; // set to [18],[45] or [18,45]
 const VACCINES = ["COVAXIN", "SPUTNIK-V"]; // include or remove whichever vaccine you want from the array - based on vaccines available on CoWin.
 const DOSE_NO = 1; //select which dose - 1 or 2
 
